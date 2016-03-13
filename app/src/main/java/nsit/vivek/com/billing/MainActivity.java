71454ebc5e8b268c9dfcc8e0249b118454099418
Vivek Bhat <vivekbhat@live.com>
2016-03-13 16:26:58 +0530

    text changed

--- app/src/main/java/nsit/vivek/com/billing/MainActivity.java
+++ app/src/main/java/nsit/vivek/com/billing/MainActivity.java
@@ -3,9 +3,8 @@ package nsit.vivek.com.billing;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -90,13 +90,13 @@ public class MainActivity extends AppCompatActivity
             startActivity(new Intent(MainActivity.this, ScrollingActivityEstimated.class));
 
         } else if (id == R.id.nav_manage) {
-            startActivity(new Intent(MainActivity.this, ScrollingActivity.class));
+
 
         } else if (id == R.id.nav_share) {
-            String message = "Hello This app has been made by Vivek ";
+            String message = "Hello This app has been made by ";
             String emailAdress[] = {"bhatvivek93@gmail.com"};
             Intent intent = new Intent(Intent.ACTION_SEND);
-           // intent.putExtra(Intent.EXTRA_TEXT, emailAdress);
+            // intent.putExtra(Intent.EXTRA_TEXT, emailAdress);
             intent.putExtra(Intent.EXTRA_SUBJECT, "Meter Reading");
             intent.setType("plain/text");
             intent.putExtra(Intent.EXTRA_TEXT, message);
